d3065add94604a1147ab2c56124e01fb6610839b
joao <joaopedroemerim@gmail.com>
2024-09-08 01:10:06 -0300

    init

---
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "binhtran432k/dracula.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
