fdc64b412f328f8bbc90b21c4815dcb79ad8152a
joao <joaopedroemerim@gmail.com>
2025-01-05 22:55:36 -0300

    add ddown alias

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -4,6 +4,7 @@ alias ls='exa -la'
 alias dbuild='docker compose up --build'
 alias dup='docker compose up'
 alias dstop='docker stop $(docker ps -a -q)'
+alias ddown='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
 
 # Neovim
 alias fnvim='nvim $(fzf -m --preview="bat --color=always {}")'
