848da8cd3ed27f4509a47731b04c10f974b05a49
Joao <joaopedroemerim@gmail.com>
2025-04-05 16:55:00 -0300

    update fish asdf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,19 @@ starship init fish | source
 zoxide init fish | source
 fzf --fish | source
 
-source /opt/asdf-vm/asdf.fish
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 export PATH="$HOME/.cargo/bin:$PATH"
 
