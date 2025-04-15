a74cc06b85955c241d9148e207bea20783773409
Joao <joaopedroemerim@gmail.com>
2025-04-14 21:52:51 -0300

    add qutebrowser config

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,11 @@
+import dracula.draw
+
+# Load existing settings made via :set
+config.load_autoconfig()
+
+dracula.draw.blood(c, {"spacing": {"vertical": 6, "horizontal": 8}})
+c.colors.webpage.darkmode.enabled = True
+
+c.url.start_pages = ["https://web.whatsapp.com/", "qute://bookmarks/"]
+c.url.searchengines = {"DEFAULT": "https://www.google.com/search?q={}"}
+c.downloads.remove_finished = 2000
