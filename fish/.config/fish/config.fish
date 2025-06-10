a93172b6a079acf5d2ae00d9c9f358b2fc79baa2
Joao <joaopedroemerim@gmail.com>
2025-06-09 22:33:42 -0300

    remove comments

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,8 +9,6 @@ else
     set _asdf_shims "$ASDF_DATA_DIR/shims"
 end
 
-# Do not use fish_add_path (added in Fish 3.2) because it
-# potentially changes the order of items in PATH
 if not contains $_asdf_shims $PATH
     set -gx --prepend PATH $_asdf_shims
 end
